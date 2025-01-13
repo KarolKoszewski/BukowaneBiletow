541ea6879181cfb041d314304f93b3866f6dd51d
Karol <karol144y@gmail.com>
2025-01-13 19:04:07 +0100

    Edytuje plik IConcert, kończe plik VipConcert

--- BukowanieBiletow/IConcert.cs
+++ BukowanieBiletow/IConcert.cs
@@ -6,8 +6,22 @@ public interface IConcert
     DateTime Date { get; }
     string Location { get; }
     int TotalNumberOfSeats { get; }
-    int AvailableSeats { get; }
+    int AvailableSeats { get; set; }
     int Price { get; }
 
-    public void bookTicket();
+    public void bookTicket()
+    {
+        if (AvailableSeats > 0)
+        {
+            Console.WriteLine("How many tickets do you want to book?");
+            int numberOfTickets = int.Parse(Console.ReadLine());
+            AvailableSeats=AvailableSeats-numberOfTickets;
+            Console.WriteLine($"You've booked: {numberOfTickets}! Available seats: {AvailableSeats}");
+            
+        }
+        else
+        {
+            Console.WriteLine("No available seats!");
+        }
+    }
 }
--- BukowanieBiletow/VipConcert.cs
+++ BukowanieBiletow/VipConcert.cs
@@ -6,10 +6,31 @@ public class VipConcert : IConcert
     public DateTime Date { get; }
     public string Location { get; }
     public int TotalNumberOfSeats { get; }
-    public int AvailableSeats { get; }
+    public int AvailableSeats { get; set; }
     public int Price { get; }
+
+    public VipConcert(string name, DateTime date, string location, int totalNumberOfSeats, int availableSeats, int price){
+        Name = name;
+        Date = date;
+        Location = location;
+        TotalNumberOfSeats = totalNumberOfSeats;
+        AvailableSeats = availableSeats;
+        Price = price;
+    }
+    
     public void bookTicket()
     {
-        throw new NotImplementedException();
+        if (AvailableSeats > 0)
+        {
+            Console.WriteLine("How many tickets do you want to book?");
+            int numberOfTickets = int.Parse(Console.ReadLine());
+            AvailableSeats=AvailableSeats-numberOfTickets;
+            Console.WriteLine($"You've booked: {numberOfTickets}! Available seats: {AvailableSeats}");
+            
+        }
+        else
+        {
+            Console.WriteLine("No available seats!");
+        }
     }
 }
