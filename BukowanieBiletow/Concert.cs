a47aba0f816529f9630b7d969176cbd506b86c46
Karol <karol144y@gmail.com>
2025-01-13 18:45:13 +0100

    Tworze Interface i klase Concert, które narazie pobierają dane o koncertach

---
+++ BukowanieBiletow/Concert.cs
@@ -0,0 +1,21 @@
+﻿namespace BukowanieBiletow;
+
+public class Concert : IConcert
+{
+    public string Name { get; }
+    public DateTime Date { get; }
+    public string Location { get; }
+    public int TotalNumberOfSeats { get; }
+    public int AvailableSeats { get; }
+    public int Price { get; }
+
+    public Concert(string name, DateTime date, string location, int totalNumberOfSeats, int availableSeats, int price)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        TotalNumberOfSeats = totalNumberOfSeats;
+        AvailableSeats = availableSeats;
+        Price = price;
+    }
+}
---
+++ BukowanieBiletow/IConcert.cs
@@ -0,0 +1,11 @@
+﻿namespace BukowanieBiletow;
+
+public interface IConcert
+{
+    string Name { get; }
+    DateTime Date { get; }
+    string Location { get; }
+    int TotalNumberOfSeats { get; }
+    int AvailableSeats { get; }
+    int Price { get; }
+}
