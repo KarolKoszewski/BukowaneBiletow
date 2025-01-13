05d7a455f3b29ecd4dbf99a182f0be35b1588fc6
Karol <karol144y@gmail.com>
2025-01-13 19:00:17 +0100

    Towrze nowy plik i impentuje wszystkie własciowsci z intrefejsu

---
+++ BukowanieBiletow/VipConcert.cs
@@ -0,0 +1,15 @@
+﻿namespace BukowanieBiletow;
+
+public class VipConcert : IConcert
+{
+    public string Name { get; }
+    public DateTime Date { get; }
+    public string Location { get; }
+    public int TotalNumberOfSeats { get; }
+    public int AvailableSeats { get; }
+    public int Price { get; }
+    public void bookTicket()
+    {
+        throw new NotImplementedException();
+    }
+}
